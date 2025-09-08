b787a9964628151b4770f8846c2ad7f38e9e40bb
kassushale1980 <kassushale@gmail.com>
2025-09-07 23:07:02 -0400

    banner

---
+++ netflix-clone/netflix-clone/src/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
+import Banner from '../components/Banner/banner'
+const Home = () => {
+  return (
+    <>
+    <Header/>
+    <Banner/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Home
